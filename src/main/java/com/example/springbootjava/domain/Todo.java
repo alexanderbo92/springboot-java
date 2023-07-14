1d59a7bf23f21d146f54a2dd6b8c1c3f9159f27e
alexanderbo92 <48901007+alexanderbo92@users.noreply.github.com>
2023-07-14 01:02:39 -0500

    Update Todo.java

--- src/main/java/com/example/springbootjava/domain/Todo.java
+++ src/main/java/com/example/springbootjava/domain/Todo.java
@@ -5,12 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Table(name = "todos")
-@Entity
 public class Todo {
-    @Id
-    @GeneratedValue
-    private Long id;
     
     private String content;
     
@@ -18,14 +13,6 @@ public class Todo {
     
     public Todo(String content) {
         this.content = content;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getContent() {
