d1f9ccd2e2524e6789f55bec474a8c8620cf6d9b
alexanderbo92 <48901007+alexanderbo92@users.noreply.github.com>
2023-07-14 01:01:45 -0500

    Update TodoRepository.java

--- src/main/java/com/example/springbootjava/repository/TodoRepository.java
+++ src/main/java/com/example/springbootjava/repository/TodoRepository.java
@@ -4,4 +4,8 @@ import com.example.springbootjava.domain.Todo;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface TodoRepository extends CrudRepository<Todo, Long> { }
+public interface TodoRepository {
+
+  test();
+  
+}
